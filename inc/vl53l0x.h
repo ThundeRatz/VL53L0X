5350618d355cb30358c9d6b5aa1b73722d758a47
Lucas Schneider <lucastrschneider@gmail.com>
2019-10-03 22:18:44 -0300

    Run uncrustify and add documentation

--- inc/vl53l0x.h
+++ inc/vl53l0x.h
@@ -3,7 +3,9 @@
  *
  * @brief User functions to deal with ST's VL53L0X API
  *
- * @date 08/2019
+ * @author Lucas Schneider <lucas.schneider@thunderatz.org>
+ *
+ * @date 10/2019
  */
 
 #ifndef VL53L0X_H
@@ -22,33 +24,76 @@
  * Public Types
  *****************************************/
 
-typedef struct _calibration_data  {
+typedef struct _calibration_data {
     uint32_t refSpadCount;
-    uint8_t isApertureSpads;
-    uint8_t VhvSettings;
-    uint8_t PhaseCal;
+    uint8_t  isApertureSpads;
+    uint8_t  VhvSettings;
+    uint8_t  PhaseCal;
 } vl53l0x_calibration_data_t;
 
 /*****************************************
  * Public Functions Prototypes
  *****************************************/
 
+/**
+ * @brief Initizalize VL53L0X device
+ *
+ * @note Before initialization, check if the device can communicate with @ref vl53l0x_wait_boot
+ *
+ * @param p_device      Device handle
+ * @param device_info   Device informations
+ * @param calibration   Device calibration data
+ *
+ * @return Error code
+ * @retval VL53L0X_ERROR_NONE   Success
+ * @retval "Other error code"   More details in @ref VL53L0X_Error
+ */
 VL53L0X_Error vl53l0x_init(VL53L0X_Dev_t* p_device, VL53L0X_DeviceInfo_t device_info,
                            vl53l0x_calibration_data_t calibration);
 
-
-uint8_t vl53l0x_update_range(VL53L0X_Dev_t* p_device, VL53L0X_RangingMeasurementData_t* p_ranging_data,
-                            uint16_t* range, uint16_t max_range);
-
-
+/**
+ * @brief Turn off device with the XSHUT pin
+ *
+ * @param p_device      Device handle
+ */
 void vl53l0x_turn_off(VL53L0X_Dev_t* p_device);
 
+/**
+ * @brief Turn on device with the XSHUT pin
+ *
+ * @param p_device      Device handle
+ */
 void vl53l0x_turn_on(VL53L0X_Dev_t* p_device);
 
+/**
+ * @brief Turn on device and check I2C communication
+ *
+ * @param p_device      Device handle
+ *
+ * @return Error code
+ * @retval VL53L0X_ERROR_NONE   Success
+ * @retval "Other error code"   More details in @ref VL53L0X_Error
+ */
 VL53L0X_Error vl53l0x_wait_boot(VL53L0X_Dev_t* p_device);
 
-uint8_t check_API_status(); //NOT IMPLEMENTED YET
+/**
+ * @brief Update sensor range
+ *
+ * @param p_device          Device handle
+ * @param p_ranging_data    Device measurement data
+ * @param range             Adress to store the measurement in mm
+ * @param max_range         Max range the sensor can read in mm
+ *
+ * @return Error code
+ * @retval 0                        Success
+ * @retval "Number between 1 and 5" Range status from sensor reading @see API_User_Manual
+ * @retval "Other error code"       More details in @ref VL53L0X_Error
+ */
+uint8_t vl53l0x_update_range(VL53L0X_Dev_t* p_device, VL53L0X_RangingMeasurementData_t* p_ranging_data, uint16_t* range,
+                             uint16_t max_range);
 
-uint8_t vl53l0x_reinit(); //NOT IMPLEMENTED YET
+uint8_t check_API_status(); // NOT IMPLEMENTED YET
 
-#endif
+uint8_t vl53l0x_reinit(); // NOT IMPLEMENTED YET
+
+#endif // VL53L0X_H
