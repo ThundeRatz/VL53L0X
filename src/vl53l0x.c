934641cd5a66848794c7e69b6a6b83252b472b87
Lucas Schneider <lucastrschneider@gmail.com>
2020-02-28 12:54:23 -0300

    Calibrate constants (Iron Cup #1)

--- src/vl53l0x.c
+++ src/vl53l0x.c
@@ -24,9 +24,9 @@
  * Private Constants
  *****************************************/
 
-#define SIGMA_LIMIT_VALUE_MM 25
-#define SIGNAL_RATE_LIMIT_VALUE_MCPS 0.2F
-#define MEASUREMENT_TIME_BUDGET_US 33000
+#define SIGMA_LIMIT_VALUE_MM 18
+#define SIGNAL_RATE_LIMIT_VALUE_MCPS 0.25F
+#define MEASUREMENT_TIME_BUDGET_US 50000
 #define PRE_RANGE_PULSE_PERIOD 18     // Must be between 12 and 18
 #define FINAL_RANGE_PULSE_PERIOD 14   // Must be between 8 and 14
 
